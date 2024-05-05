0dbd65b0e8a32a5d26eb8c795e1af3242fe58240
Csakany Cristian <crist_csk@yahoo.com>
2024-05-05 19:24:20 +0300

    added tailwindcss

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="background">
+      <div className="primutext">testtest</div>
+      <div className="text-blue-500 text-6xl">testuldejos</div>
     </div>
   );
 }
